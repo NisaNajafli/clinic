976e67de169b6bb88bf8667afee895db43552a99
Nisa Najafli <ncfliniss@gmail.com>
2025-09-14 22:57:41 +0400

    update

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ mobileMenu.addEventListener('hidden.bs.offcanvas', () => {
   closeIcon.classList.add('d-none');
   hamburgerIcon.classList.remove('d-none');
 });
+
+
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
