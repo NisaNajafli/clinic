e2b3b1e4f7b27dbf21359b871f942fd8376700b9
NisaNajafli <ncfliniss@gmail.com>
2025-09-15 16:56:13 +0400

    update

--- main.js
+++ main.js
@@ -23,3 +23,8 @@ window.addEventListener("scroll", function () {
   }
 });
 
+var mainDiv = document.getElementById('main-button');
+mainDiv.addEventListener('click', function(){
+  this.children.item(0).classList.toggle('fa-times');
+  this.classList.toggle('open');
+});
